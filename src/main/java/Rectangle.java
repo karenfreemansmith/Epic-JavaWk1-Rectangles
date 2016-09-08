a705ec6a5d3154cffbe82a1a22438fd0afe759c1
Elysia Nason and Karen Freeman-Smith <karen@>
2016-09-08 09:14:27 -0700

    add sucessful second test

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,10 +1,11 @@
 public class Rectangle {
+  private int mHeight;
 
   public Rectangle(int height, int width) {
-
+    mHeight = height;
   }
 
   public int getHeight() {
-    return 0;
+    return mHeight;
   }
 }
