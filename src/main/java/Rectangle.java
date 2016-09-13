3963ba41f741fb37c11eb2e9a524d0ec64c8b17c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-13 05:55:18 -0700

    add static method to keep track of triangles

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,10 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Rectangle {
   private int mHeight;
   private int mWidth;
+  private static List<Rectangle> instances = new ArrayList<Rectangle>();
 
   public Rectangle(int height, int width) {
     mHeight = height;
     mWidth = width;
+    instances.add(this);
   }
 
   public int getHeight() {
@@ -17,5 +22,9 @@ public class Rectangle {
 
   public boolean isSquare() {
     return mHeight == mWidth;
+  }
+
+  public static List<Rectangle> all() {
+    return instances;
   }
 }
--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -32,4 +32,13 @@ public class RectangleTest {
     Rectangle testRectangle = new Rectangle(2, 2);
     assertEquals(true, testRectangle.isSquare());
   }
+
+  @Test
+  public void all_returnsAllInstancesOfRectangle_true() {
+    Rectangle firstRectangle = new Rectangle(10, 20);
+    Rectangle secondRectangle = new Rectangle(5, 5);
+    assertTrue(Rectangle.all().contains(firstRectangle));
+    assertTrue(Rectangle.all().contains(secondRectangle));
+  }
+
 }
