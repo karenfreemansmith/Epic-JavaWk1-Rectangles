d0e7c6cbc98a65cb1f877e01061cf9d5fa80f5b0
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-09-13 06:58:21 -0700

    add front-end pages to recctangles

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,32 @@
-import java.io.Console;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
-    Console c = System.console();
-    int intHeight = Integer.parseInt(c.readLine("Enter the height of your rectangle: "));
-    int intWidth = Integer.parseInt(c.readLine("Enter the width of your rectangle: "));
-    Rectangle rectangle = new Rectangle(intHeight, intWidth);
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
-    if(rectangle.isSquare()) {
-      System.out.println("Hey, that's a square!");
-    } else {
-      System.out.println("Just your typical rectangle...");
-    }
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/result", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      int intHeight = Integer.parseInt(request.queryParams("length"));
+      int intWidth = Integer.parseInt(request.queryParams("width"));
+      Rectangle rectangle = new Rectangle(intHeight, intWidth);
+      ArrayList<Rectangle> myRectangleArrayList = Rectangle.all();
+      model.put("myRectangles", myRectangleArrayList);
+      model.put("template", "templates/result.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
   }
 }
